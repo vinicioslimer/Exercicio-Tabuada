e03b9c21ae33314f3a331bb9fc37dd40385f7534
Vinicios Limer <vinicioslimer@outlook.com>
2022-09-15 17:35:40 -0300

    Código adicionado

---
+++ script.js
@@ -0,0 +1,16 @@
+function tabuar() {
+
+    let num = document.getElementById("num");
+        
+    let res = document.querySelector('div#res')
+    res.innerHTML = '';
+    
+    if (num.value.lenght == 0) {
+        alert('Por favor digite um número!')
+    } else {
+    for (let i=0; i <= 10; i++) {
+    n = Number(num.value);
+    res.innerHTML += `${n} X ${i} = ` + n*i + "<br>";
+    }
+}
+}
